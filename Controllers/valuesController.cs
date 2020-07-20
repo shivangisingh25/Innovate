c1caac6b8cf62619f1378d605a7e9b27d1afa8d8
Shivangi Singh <shivangisingh211@outlook.com>
2020-07-21 01:22:33 +0530

    Add project files.

---
+++ Controllers/valuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Innovate.Controllers
+{
+    [Route("api/[value]")]
+    [ApiController]
+    public class valuesController : ControllerBase
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
